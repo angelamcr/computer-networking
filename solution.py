5ee97cdd6466fb1b580a32cdfb61635db7ff3195
angelamcr <angelam.mendenhall@gmail.com>
2021-09-01 14:11:08 +0900

    Complete questions and answers

--- solution.py
+++ solution.py
@@ -3,17 +3,45 @@
 ### Output - The right answer for the specific question.
 
 def welcome_assignment_answers(question):
-    #The student doesn't have to follow the skeleton for this assignment.
-    #Another way to implement is using a "case" statements similar to C.
-    if question == "Are encoding and encryption the same? - Yes/No":
-        answer = "The student should type the answer here"
+    answer = ""
+    if question == "In Slack, what is the secret passphrase posted in the #cyberfellows-computernetworking-fall2021 channel posted by a TA?":
+            answer = "mTLS"
+    elif question == "Are encoding and encryption the same? - Yes/No":
+            answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "The student should type the answer here"
-    return(answer)
-# Complete all the questions.
+            answer = "No"
+    elif question == "Is it possible to decode a message without a key? - Yes/No":
+            answer = "Yes"
+    elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
+            answer = "No"
+    elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code":
+            answer = "68c74b1fe84ac8c662849915bc9002f4"
+    elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
+            answer = "No"
+    elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
+            answer = 5
+    elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
+            answer = 4
+    return answer
 
 
 if __name__ == "__main__":
     #use this space to debug and verify that the program works
+    debug_question = "In Slack, what is the secret passphrase posted in the #cyberfellows-computernetworking-fall2021 channel posted by a TA?"
+    print(welcome_assignment_answers(debug_question))
     debug_question = "Are encoding and encryption the same? - Yes/No"
+    print(welcome_assignment_answers(debug_question))
+    debug_question = "Is it possible to decrypt a message without a key? - Yes/No"
+    print(welcome_assignment_answers(debug_question))
+    debug_question = "Is it possible to decode a message without a key? - Yes/No"
+    print(welcome_assignment_answers(debug_question))
+    debug_question = "Is a hashed message supposed to be un-hashed? - Yes/No"
+    print(welcome_assignment_answers(debug_question))
+    debug_question = "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code"
+    print(welcome_assignment_answers(debug_question))
+    debug_question = "Is MD5 a secured hashing algorithm? - Yes/No"
+    print(welcome_assignment_answers(debug_question))
+    debug_question = "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number"
+    print(welcome_assignment_answers(debug_question))
+    debug_question = "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number"
     print(welcome_assignment_answers(debug_question))
