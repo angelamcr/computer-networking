bcd70d3c19ec6d733d70a3639620a7f2cbb5f05c
angelamcr <angelam.mendenhall@gmail.com>
2021-11-04 11:41:28 +0900

    Improved some functions.

--- solution.py
+++ solution.py
@@ -1,10 +1,8 @@
 ﻿from socket import *
-import os
 import sys
 import struct
 import time
 import select
-import binascii
 
 
 ICMP_ECHO_REQUEST = 8
@@ -19,18 +17,15 @@ def checksum(string):
     countTo = (len(string) // 2) * 2
     count = 0
 
-
     while count < countTo:
         thisVal = (string[count + 1]) * 256 + (string[count])
         csum += thisVal
         csum &= 0xffffffff
         count += 2
 
-
     if countTo < len(string):
         csum += (string[len(string) - 1])
         csum &= 0xffffffff
-
 
     csum = (csum >> 16) + (csum & 0xffff)
     csum = csum + (csum >> 16)
@@ -41,24 +36,26 @@ def checksum(string):
 
 
 def build_packet():
-    # Header is type (8), code (8), checksum (16), id (16), sequence (16)
+    # dummy identifier (processid/portnumber) initial checksum 0 to create the header and store it as bytes
+    code = 0
     myChecksum = 0
-    # Make a dummy header with a 0 checksum
-    # struct -- Interpret strings as packed binary data
-    ID = 60000
-    header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, ID, 1)
+    identifier = 60000
+    sequence = 1
+    header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, code, myChecksum, identifier, sequence)
+
     data = struct.pack("d", time.time())
-    # Calculate the checksum on the data and the dummy header.
+
+    # Calculate the checksum on the data and the header.
     myChecksum = checksum(header + data)
 
-    # Get the right checksum, and put in the header
+    # Convert 16-bit integers from host to network byte order
     if sys.platform == 'darwin':
-        # Convert 16-bit integers from host to network  byte order
         myChecksum = htons(myChecksum) & 0xffff
     else:
         myChecksum = htons(myChecksum)
 
-    header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, ID, 1)
+    # Create a new header with the new checksum in Big-Endian notation
+    header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, code, myChecksum, identifier, sequence)
 
     packet = header + data
     return packet
@@ -74,67 +71,65 @@ def get_route(hostname):
         for tries in range(TRIES):
             destAddr = gethostbyname(hostname)
 
-            icmp = getprotobyname("icmp")
-            mySocket = socket(AF_INET, SOCK_RAW, icmp)
-
+            mySocket = socket(AF_INET, SOCK_RAW, getprotobyname("icmp"))
             mySocket.setsockopt(IPPROTO_IP, IP_TTL, struct.pack('I', ttl))
             mySocket.settimeout(TIMEOUT)
+
             try:
-                d = build_packet()
-                # mySocket.sendto(d, (hostname, 0))
-                mySocket.sendto(d, (destAddr, 0))
-                t= time.time()
+                icmpPacket = build_packet()
+                # mySocket.sendto(icmpPacket, (hostname, 0))
+                mySocket.sendto(icmpPacket, (destAddr, 0))
                 startedSelect = time.time()
                 whatReady = select.select([mySocket], [], [], timeLeft)
                 howLongInSelect = (time.time() - startedSelect)
-                if whatReady[0] == []: # Timeout
+
+                if not whatReady[0]: # Timeout
                     tracelist1.append(str(ttl))
                     tracelist1.append("*")
                     tracelist1.append("Request timed out.")
-                    #You should add the list above to your all traces list
                     tracelist2.append(tracelist1)
                 recvPacket, addr = mySocket.recvfrom(1024)
                 timeReceived = time.time()
                 timeLeft = timeLeft - howLongInSelect
+
                 if timeLeft <= 0:
-                    # tracelist1.append("* * * Request timed out.")
                     tracelist1.append(str(ttl))
                     tracelist1.append("*")
                     tracelist1.append("Request timed out.")
-                    #You should add the list above to your all traces list
                     tracelist2.append(tracelist1)
             except timeout:
                 continue
 
 
             else:
-                #Fetch the icmp type from the IP packet
-                types = struct.unpack("b", recvPacket[20:21])[0]
+                # Fetch the icmp type from the IP packet
+                # type field is the first after the IP header
+                type = struct.unpack("b", recvPacket[20:21])[0]
                 try:
                     responseHostName = gethostbyaddr(addr[0])
                 except herror:
                     responseHostName = "hostname not returnable"
 
-                if types == 11:
-                    bytes = struct.calcsize("d")
-                    timeSent = struct.unpack("d", recvPacket[28:28 +
+                if type == 11:
+                    bytes = struct.calcsize("icmpPacket")
+                    timeSent = struct.unpack("icmpPacket", recvPacket[28:28 +
                     bytes])[0]
                     tracelist1.append(str(ttl))
                     tracelist1.append(str((timeReceived - timeSent) * 1000))
                     tracelist1.append(addr[0])
                     tracelist1.append(responseHostName)
                     tracelist2.append(tracelist1)
-                elif types == 3:
-                    bytes = struct.calcsize("d")
-                    timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
+                elif type == 3:
+                    bytes = struct.calcsize("icmpPacket")
+                    timeSent = struct.unpack("icmpPacket", recvPacket[28:28 + bytes])[0]
                     tracelist1.append(str(ttl))
                     tracelist1.append(str((timeReceived - timeSent) * 1000))
                     tracelist1.append(addr[0])
                     tracelist1.append(responseHostName)
                     tracelist2.append(tracelist1)
-                elif types == 0:
-                    bytes = struct.calcsize("d")
-                    timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
+                elif type == 0:
+                    bytes = struct.calcsize("icmpPacket")
+                    timeSent = struct.unpack("icmpPacket", recvPacket[28:28 + bytes])[0]
                     tracelist1.append(str(ttl))
                     tracelist1.append(str((timeReceived - timeSent) * 1000))
                     tracelist1.append(addr[0])
